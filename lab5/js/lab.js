51320e7759f1441b0f66f82b1ffa11078865e334
Kam-R4 <kren4@ucsc.edu>
2024-10-21 16:39:11 -0700

    lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,9 +1,22 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Kam Ren
+// Date: 21 October 
 
-// Constants
+// Define Variable 
+make = "Toyota";
+model = "Corolla";
+color = "Gray";
+year = "2016";
 
+//calculate 
+age = 2019- year;
+
+//output
+document.writeln("Make: " + make + "<br>")
+document.writeln("Model: " + model + "<br>")
+document.writeln("Color: " + color + "<br>")
+document.writeln("Year: " + year + "<br>")
+document.writeln("Age: " + age + " years<br> ")
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
