55d528f5352a5a978114bb6ee25277a365259265
Kam-R4 <kren4@ucsc.edu>
2024-11-18 17:43:47 -0800

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -7,33 +7,41 @@
 // Functions
 
 function oneLongString(str){
+  let result = "";
   for (i=1; i <= 200; i++) { 
     //if multiple of 15  
    if (i% 15 == 0) {
-     console.log("FizzBuzz");
+     //console.log("FizzBuzz");
+     result += "FizzBuzz<br>";
   
    } 
    //if multiple of 5
    else if(i% 7 == 0) { 
-    console.log("Boom");
+    //console.log("Boom");
+    result += "Boom<br>";
   } 
    else if (i% 5 == 0) { 
-     console.log("Buzz");
+     //console.log("Buzz");
+     result += "Buzz<br>";
    } 
     //if multiple of 3
    else if (i% 3 == 0) {
-     console.log("Fizz");
+     //console.log("Fizz");
+     result += "Fizz<br>";
    } else {
-     console.log(i);
+     //console.log(i);
+     result += i + "<br>";
+     
    }
   }
+  return result;
 
 };
 
 
 $("#my-button").click(function(){
 
-  $("#output").html(oneLongString);
+  $("#output").html(oneLongString());
  
     
 });
