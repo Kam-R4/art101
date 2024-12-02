37671ec33b52519ddd267bb773d671b6ec023549
Kam-R4 <kren4@ucsc.edu>
2024-12-01 21:47:13 -0800

    lab16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -7,15 +7,25 @@
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Using the core $.ajax() method
+$.ajax({
+  // The URL for the request (from the api docs)
+  url: "https://cors-anywhere.herokuapp.com/https://xkcd.com/3018/info.0.json",
+  // Whether this is a POST or GET request
+  type: "GET",
+  // The type of data we expect back
+  //dataType : "jsonp",
+  dataType: "json", 
+  // What do we do when the api call is successful
+  //   all the action goes in here
+  success: function(data) {
+      // do stuff
+      console.log(data);
+    $("#output").append("<img src='" + data.img + "'>")
+  },
+  // What we do if the api call fails
+  error: function (jqXHR, textStatus, errorThrown) { 
+      // do stuff
+      console.log("Error:", textStatus, errorThrown);
+  }
+})
